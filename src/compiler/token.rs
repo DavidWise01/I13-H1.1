389879b23f3b4dfed7449b421be9968f131bbce0
reaper <148925959+DavidWise01@users.noreply.github.com>
2026-08-16 20:14:09 -0500

    compiler: add token model

---
+++ src/compiler/token.rs
@@ -0,0 +1,36 @@
+use super::source::Span;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenKind {
+    I,
+    Def,
+    If,
+    Ident(String),
+    Number(f64),
+    LBrace,
+    RBrace,
+    LParen,
+    RParen,
+    Comma,
+    Dot,
+    Bind,
+    ReturnArrow,
+    EqEq,
+    NotEq,
+    Lt,
+    Lte,
+    Gt,
+    Gte,
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Newline,
+    Eof,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Token {
+    pub kind: TokenKind,
+    pub span: Span,
+}
